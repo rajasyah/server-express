090374266c9b89238d6730a5dc9f0b7f230929ab
rajasyah <rajasyahacp20@gmail.com>
2021-10-18 00:12:07 +0700

    setting port env

--- app.js
+++ app.js
@@ -15,4 +15,6 @@ app.get("/about", (req, res) => {
 
 app.use(userRouter);
 
-app.listen(3000, console.log("server running at http://localhost:3000"));
+app.listen(process.env.PORT || 3000, () =>
+  console.log("server running at http://localhost:3000")
+);
